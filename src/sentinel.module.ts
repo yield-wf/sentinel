ed7883a0af589d409d72c706a5c5e98010dee8ea
Luiz Gustavo Freire Gama <luizgama@onrails.com.br>
2019-12-17 13:38:56 -0300

    fix: comment code while nestjs adapter is pending

--- src/sentinel.module.ts
+++ src/sentinel.module.ts
@@ -1,20 +1,21 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { SentinelService } from './sentinel';
+// TODO: Add NestJS adapter
+// import { DynamicModule, Module } from '@nestjs/common';
+// import { SentinelService } from './sentinel';
 
-@Module({})
-export class SentinelModule {
-    static forRoot(): DynamicModule {
-        const providers = [
-            {
-                provide: SentinelService,
-                useValue: new SentinelService
-            },
-        ];
+// @Module({})
+// export class SentinelModule {
+//     static forRoot(): DynamicModule {
+//         const providers = [
+//             {
+//                 provide: SentinelService,
+//                 useValue: new SentinelService
+//             },
+//         ];
 
-        return {
-            providers: providers,
-            exports: providers,
-            module: SentinelModule,
-        };
-    }
-}
+//         return {
+//             providers: providers,
+//             exports: providers,
+//             module: SentinelModule,
+//         };
+//     }
+// }
